3b7eebe781401f86b44f9de49d74ba382ee67ec1
Mohamad-Hag <mohamadhag99@gmail.com>
2021-05-12 15:38:03 +0300

    Commit 21

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 let difficulty = "easy";
+let currentSelectedIndex = 0;
+
+window.onload = () => {
+  document.querySelector("#difficulty-options").focus();
+};
 
 function goToPlayground() {
   window.location.href = "pages/playground.html?difficulty=" + difficulty;
 }
-function difficultyOptionClicked(e)
-{
+function difficultyOptionClicked(e) {
   let options = document.querySelectorAll(".difficulty-option");
-  options.forEach((option) => {
+  options.forEach((option, i) => {
     option.removeAttribute("id");
+    if (option === e) currentSelectedIndex = i;
   });
   e.setAttribute("id", "difficulty-option-selected");
   difficulty = e.innerText.toLowerCase();
+}
+function difficultyOptionKeyDown(e) {
+  let option = document.querySelectorAll(".difficulty-option");
+  // Up Arrow
+  if (e.keyCode === 38) {
+    if (currentSelectedIndex > 0) {
+      currentSelectedIndex--;
+      difficultyOptionClicked(option[currentSelectedIndex]);
+    } else {
+      difficultyOptionClicked(option[2]);
+    }
+  }
+  // Down Arrow
+  else if (e.keyCode === 40) {
+    if (currentSelectedIndex < 2) {
+      currentSelectedIndex++;
+      difficultyOptionClicked(option[currentSelectedIndex]);
+    } else {
+      difficultyOptionClicked(option[0]);
+    }
+  }
+  // Enter Key
+  else if (e.keyCode === 13) {
+    goToPlayground();
+  }
 }
